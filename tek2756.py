dfbc1e7628e955ff13f7d08f9c942e4d4b08e817
Adam R. Maxwell <amaxwell@mac.com>
2026-04-11 20:21:47 -0700

    add comments, remove obsolete main implementation

--- tek2756.py
+++ tek2756.py
@@ -1,5 +1,15 @@
 #!/Library/Frameworks/Python.framework/Versions/3.14/bin/python3.14
 
+#
+# Requires pyvisa, and I'm using pyvisa-py to connect to my SA over ethernet
+# using a Tek AD007 GPIB-ethernet bridge. I haven't tried to make it work
+# with AR488, as part of the motivation for this is to avoid the timeout
+# limitations of AR488, especially when used with John Miles' phase noise
+# program.
+#
+# 
+#
+
 import pyvisa
 import time
 from struct import *
@@ -200,127 +210,20 @@ class Tektronix2756P(object):
 
 
 if __name__ == '__main__':
-    
-    from math import log10, pow
-    import sys
-    
+        
     sa = Tektronix2756P()
     sa.save_state()
     
-    #sa.set_center_frequency(100, "MHZ")
-    #scaled_x, scaled_y = sa.curve()
-    
-    pn_x = []
-    pn_y = []
+    sa.set_center_frequency(100, "MHZ")
     
     min_offset = 100
     assert min_offset >= 100, "starting offset from carrier must be at least 100 Hz"
     max_offset = 1e6
     nominal_carrier = 100e6
     carrier = nominal_carrier
-    retune_carrier = True
     
-    #carrier, carrier_level = sa.carrier_near(carrier)
-    #print("found carrier:", carrier, carrier_level)
-    
-    # center frequency; we start measuring at the offset, not at the peak,
-    # so we tune to the right of the carrier
-    tune_freq = carrier + min_offset
-    
-    # FIXME: use measured?
-    carrier_level = -20
-    sa.set_reflevel(carrier_level)
-    sa.set_vbw("0")
-    
-    # should be negative or zero
-    clip = -30
-    
-    if clip and carrier_level < 0:
-        sa.set_reflevel(carrier_level + clip)
-        print("Clipping signal with ref level:", carrier_level + clip)
-    elif clip:
-        assert carrier_level < 0, "refusing to clip carrier at level 0 dBm or greater" 
+    carrier, carrier_level = sa.carrier_near(carrier)
+    print("found carrier:", carrier, carrier_level)
 
-    min_decade = int(log10(min_offset) + 0.5)
-    max_decade = int(log10(max_offset) + 0.5)
-    #print("decades:", min_decade, max_decade)
-   
-    #print("min offset Hz", pow(10.0, min_decade) + 0.5)
-    #print("max offset Hz", pow(10.0, max_decade + 1) + 0.5)
-    
-    pn_x = []
-    pn_y = []
-    
-    # list of 2, 3, 4, 5, etc; not a starting frequency
-    for current_decade in range(min_decade, max_decade):
-           
-        print("xxxxxxxxxxxxxxx")
-
-        # 100-1,000, 1,000-10,000, 10,000-100,000
-        # decade_start is our offset from the carrier, and it will be a decade wide
-        decade_start = int(pow(10.0, current_decade) + 0.5)        
-        total_span = decade_start * 10
-    
-        rbw = int(decade_start / 10)
-        sa.set_rbw(rbw)
-   
-        print("decade %s Hz to %s Hz offset" % (decade_start, decade_start + total_span))
-        print("RBW: %s Hz, Span/Div: %s Hz" % (rbw, total_span / 10))
-    
-        # set_span requires span/div
-        sa.set_span(total_span / 10, units="HZ")
-    
-        # jog frequency by half the total span (starts at carrier + decade_start)
-        # check carrier and retune each decade in case of slow drift
-        if retune_carrier:
-            carrier, measured_carrier_level = sa.carrier_near(carrier)
-        
-            # hit this with the HP 8620C; drifted so far I couldn't find it
-            assert abs(abs(carrier_level) - abs(measured_carrier_level)) < 10, "no carrier detected within 10 dB of nominal value"
-            
-            # warn in case of drift; tried 10 Hz and hit that immediately with
-            # an old Tek 067-0532-00 leveled sine generator
-            if abs(carrier - nominal_carrier) > total_span / 4:
-                sys.stderr.write("*** WARNING *** Carrier drifted more than %d Hz from nominal: %d\n" % (total_span/4, carrier))
-        
-        center_frequency = carrier + decade_start + total_span/2
-        sa.set_center_frequency(center_frequency, units="HZ")
-        print("center at %s for carrier at %s" % (center_frequency, carrier))
-    
-        scaled_x, scaled_y = sa.curve()
-    
-        # HP uses -10 * log10(1.2 * rbw) as starting estimate. KE5FX uses an additional
-        # additive factor, but not the 1.2. This just confused me, so I pull the cal
-        # constants for this machine and use them directly.
-
-        # Need to subtract this value of noise bandwidth correction (in dB, per manual),
-        # which matches the HP PN seminar notes. Had the sign wrong here initially and
-        # got super confused because the values are so much larger than the ones in pn.cpp.
-        f_corr = -1 * sa.filter(rbw).noise_bandwidth_F
-        print("F corr dB:", f_corr)
-    
-        # FIXME check this; HP default value. Probably reasonable, given how close
-        # the -10 * log10(1.2 * rbw) value is to our actual constants. KE5FX says
-        # the Tek SA's track this internally, so it should be zero.
-        Cn = 0
-            
-        # Extend the previous results; sort at the end because of overlap.
-        # Compute frequency offset relative to most recently measured carrier
-        # in case retune_carrier is true.
-        for sx, sy in zip(scaled_x, scaled_y):
-            pn_x.append(sx - carrier)
-            pn_y.append(sy - carrier_level + f_corr + Cn)
-        
-    with open("/tmp/tek2756_test.csv", "w") as outf:
-    
-        # header disabled for now as it screws up my copy/paste to DataGraph
-        #outf.write("f (Hz),L (dBc/Hz)\n")    
-        
-        # sort the list by frequency on writing, since they are
-        # overlapping
-        # https://www.reddit.com/r/learnprogramming/comments/91bl6v/python_sort_multiple_lists_based_on_the_sorting/
-         
-        for x, y in sorted(zip(pn_x, pn_y)):
-            outf.write("%f,%f\n" % (x,y))
-            
+                
     sa.restore_state()
